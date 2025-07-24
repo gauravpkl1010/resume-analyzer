a46bf44bb8a98a0d5a462b37b3ebcbf185a7b138
gaurav <pyakurelgaurav01@gmail.com>
2025-07-24 07:03:12 +0545

    auth completed

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,29 +1,29 @@
+import { usePuterStore } from "~/lib/puter";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router";
-import { usePuterStore } from "~/lib/puter";
 
-export const meta = () => {
-  return [
-    { title: "VITAESCAN | Auth" },
-    { name: "description", content: "Log into your Account." },
-  ];
-};
+export const meta = () => [
+  { title: "VITAESCAN | Auth" },
+  { name: "description", content: "Log into your account" },
+];
 
 const Auth = () => {
   const { isLoading, auth } = usePuterStore();
   const location = useLocation();
   const next = location.search.split("next=")[1];
   const navigate = useNavigate();
+
   useEffect(() => {
     if (auth.isAuthenticated) navigate(next);
-  }, [auth.isAuthenticated, next, navigate]);
+  }, [auth.isAuthenticated, next]);
+
   return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
       <div className="gradient-border shadow-lg">
         <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
           <div className="flex flex-col items-center gap-2 text-center">
             <h1>Welcome</h1>
-            <h3>Login to Continue Your Journey</h3>
+            <h2>Log In to Continue Your Journey</h2>
           </div>
           <div>
             {isLoading ? (
@@ -49,4 +49,5 @@ const Auth = () => {
     </main>
   );
 };
+
 export default Auth;
